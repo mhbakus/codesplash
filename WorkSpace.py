52bd2b9bf9abb387a8235cc092ac309b5013b1f0
Elkana Rop <elkanarop@gmail.com>
2017-10-04 10:24:45 +0000

    can now write to a csv file

--- WorkSpace.py
+++ WorkSpace.py
@@ -1,16 +1,17 @@
-import smtplib, random, csv
+import csv, os, random, smtplib
 
 class Verification:
 	def __init__(self):
 		self.random_key = ''
 
+	#used to generate random numbers which are returned with this function test
 	def generate_random_numbers(self):
 		s=list(range(6))
 		random.shuffle(s) # << shuffle before print or assignment
 		self.random_key = "".join(str(x) for x in s)
 		return self.random_key
 
-
+	#used to send the random number to the email of the account user
 	def send_notification(self, receiver_email):
 		gmail_mail_config = "elkana@meltwater.org"
 		gmail_password_config = "elykips+254"
@@ -25,6 +26,7 @@ class Verification:
 		mail.close()
 		print("Succesfully sent, Please check email for confirmation code")
 
+	#used to verify the code entered to make sure it is same as the one sent
 	def verify_code(self, code_received):
 		if code_received == self.random_key:
 			return True
@@ -37,6 +39,7 @@ class WorkSpaceAdmin:
 		self.admins_dict = admins_dict
 		self.admin_list = admin_list
 
+	#use to create the admin of the workspace
 	def create_admin_dict(self, fullname, username, password, email):
 		self.admins_dict['fullname'] = fullname
 		self.admins_dict['username'] = username
@@ -60,40 +63,34 @@ class WorkSpace(WorkSpaceAdmin):
 		self.size_of_team = 0
 		self.user_role = ''
 
+	#used to write to the csv the content of the class that was created
 	def create_work_space(self, name, team_purpose, size_of_team, email):
-		WorkSpace.work_dict['name'] = name
-		# if self.team_purpose == '1':
-		# 	WorkSpace.work_dict['team_purpose'] = 'shool'
-		# elif self.team_purpose == '2':
-		# 	WorkSpace.work_dict['team_purpose'] = 'work'
-		# elif self.team_purpose == '3':
-		# 	WorkSpace.work_dict['team_purpose'] = 'shared interest'
-		# elif self.team_purpose == '4':
-		# 	WorkSpace.work_dict['team_purpose'] = 'others'
-		WorkSpace.work_dict['size_of_team'] = size_of_team
-		WorkSpace.work_dict['admin_mail'] = email
-		WorkSpace.work_dict['team_purpose'] = team_purpose
-		WorkSpace.work_dict['url'] = name + ".slack.com"
-		# f = open('workspace.csv','w')
-		# f.write(str(WorkSpace.work_dict)) 
-		# f.close()
-
-		fieldnames = ["WorkSpaceName", "TeamSize", "Email", "Purpose", "Url"]
-		writer = csv.DictWriter(newfile, fieldnames=fieldnames)
-		for row in WorkSpace.work_dict:
-			writer.writerow(row)
+		self.url = name + ".slack.com"
+		has_header = False
+		with open('workspace.csv', 'r') as f:
+			for line in f:
+				if len(line) >= 1:
+					has_header = True
+					break
+				else:
+					pass
 
 
-		WorkSpace.work_list.append(WorkSpace.work_dict)
+		with open('workspace.csv', 'a') as file:
+			messages = csv.DictWriter(file, fieldnames = ["WorkSpaceName", "TeamSize", "Email", "Purpose", "Url"])
+			if not has_header:
+				messages.writeheader()
+			messages.writerow({'WorkSpaceName': name, 'TeamSize': size_of_team, 'Email': email, 'Purpose': team_purpose, 'Url': self.url })
+			#WorkSpace.work_list.append(WorkSpace.work_dict)
 
 	def get_workspaces(self):
 		return WorkSpace.work_list
 	
 
 if __name__ == "__main__":
-	verification = Verification()
-	work_admin = WorkSpaceAdmin()
-	workspace = WorkSpace()
+	verification = Verification()#create an instance of the verification
+	work_admin = WorkSpaceAdmin()#cerate an instance of the work_admin
+	workspace = WorkSpace()#create an instance of the workspace
 
 	email = input("Enter Email: ")
 	verification.generate_random_numbers()
@@ -126,7 +123,7 @@ if __name__ == "__main__":
 		name_of_coy = input(" Company Name: ")
 		workspace.create_work_space(name_of_coy, purpose, size_of_team, email)
 
-		print(workspace.get_workspaces())
+		#print(workspace.get_workspaces())
 	else:
 		print("gerarahia mehn")
 
